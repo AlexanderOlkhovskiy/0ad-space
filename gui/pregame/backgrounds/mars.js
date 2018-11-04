84657e8a78773d4a70a6933f660224038ca4bd3c
vladislavbelov <vladislavbelovdev@gmail.com>
2018-11-04 21:39:55 +0300

    Add new background with Mars rover.
    Author: astarcis (https://www.deviantart.com/astarcis).

--- gui/pregame/backgrounds/mars.js
+++ gui/pregame/backgrounds/mars.js
@@ -1,14 +1,26 @@
+let time_scale = 0.05;
+
 g_BackgroundLayerData.push(
 	[
 		{
-			"offset": (time, width) => 0.10 * width * Math.cos(0.04 * time),
+			"offset": (time, width) => -width * 0.65 + 0.05 * width * Math.sin(time * time_scale),
 			"sprite": "background-mars1_1",
 			"tiling": true,
 		},
 		{
-			"offset": (time, width) => 0.17 * width * Math.cos(0.05 * time) + width / 8,
+			"offset": (time, width) => -width * 0.65 + 0.16 * width * Math.sin(time * time_scale),
 			"sprite": "background-mars1_2",
-			"tiling": false,
+			"tiling": true,
+		},
+		{
+			"offset": (time, width) => -width * 0.65 + 0.23 * width * Math.sin(time * time_scale),
+			"sprite": "background-mars1_3",
+			"tiling": true,
+		},
+		{
+			"offset": (time, width) => -width * 0.65 + 0.28 * width * Math.sin(time * time_scale),
+			"sprite": "background-mars1_4",
+			"tiling": true,
 		}
 	]
 );
